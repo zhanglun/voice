f29bc5a8624389f8de3c6f2ab2f478ac6625c469
zhanglun <zhanglun1410@gmail.com>
2018-01-24 23:34:32 +0800

    toggle sidebar

--- app/components/Home.js
+++ app/components/Home.js
@@ -19,27 +19,33 @@ export default class Home extends Component<Props> {
     this.state = {
       openSidebar: false,
     };
+
+    this.toggleSidebar = this.toggleSidebar.bind(this);
+  }
+
+  toggleSidebar() {
+    this.setState({ openSidebar: !this.state.openSidebar });
   }
 
   render() {
     return (
       <div className={styles.container}>
         <Header />
-        <ReactCSSTransitionGroup
-          transitionName="example"
-          transitionEnterTimeout={500}
-          transitionLeaveTimeout={300}
-        >
-          <Sidebar />
-        </ReactCSSTransitionGroup>
+        {/*<ReactCSSTransitionGroup*/}
+          {/*transitionName="example"*/}
+          {/*transitionEnterTimeout={500}*/}
+          {/*transitionLeaveTimeout={300}*/}
+        {/*>*/}
+        <Sidebar show={this.state.openSidebar} handleToggle={this.toggleSidebar} />
+        {/*</ReactCSSTransitionGroup>*/}
         <div className={styles.inner} data-tid="container">
           <h2>Home</h2>
-          <span>{this.state.openSidebar}</span>
+          <span data-tclass="btn" onClick={this.toggleSidebar}>adfadfadf{this.state.openSidebar}</span>
           <Link to="/vols">to Vols</Link>
           <Link to="/">to Home</Link>
           <Link to="/counter">to Counter</Link>
-          <Route path="/vols" component={VolsPage}/>
-          <Route path="/counter" component={CounterPage}/>
+          <Route path="/vols" component={VolsPage} />
+          <Route path="/counter" component={CounterPage} />
         </div>
       </div>
     );
--- app/components/Sidebar/Sidebar.js
+++ app/components/Sidebar/Sidebar.js
@@ -3,16 +3,32 @@ import { route, Link } from 'react-router-dom';
 
 import style from './Sidebar.less';
 
-type Props = {};
+type Props = {
+  show: boolean,
+  handleToggle: () => void
+};
 
 export default class Sidebar extends Component<Props> {
   props: Props;
 
+  constructor(props) {
+    super(props);
+
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+    this.props.handleToggle();
+  }
+
   render() {
+    const { show } = this.props;
+
     return (
-      <div className={style.container}>
+      <div className={`${style.container} ${show ? style.show : ''}`} onClick={this.handleClose}>
         lalla用这个开源组件 https://github.com/negomi/react-burger-menu
       </div>
     );
   }
 }
+
