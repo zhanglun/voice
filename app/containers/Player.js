662b0b3b29f646c6c43d46db6a55d761ec644210
zhanglun <zhanglun1410@gmail.com>
2018-02-12 15:34:00 +0800

    增加palyer

---
+++ app/containers/Player.js
@@ -0,0 +1,18 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import Player from '../components/Player';
+import * as PlayerActions from '../actions/player';
+
+
+function mapStateToProps(state) {
+  return {
+    player: state.player,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(PlayerActions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
