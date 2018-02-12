a20dcff44e1c89f105955025eb2a79a8d5d0c30a
zhanglun <zhanglun1410@gmail.com>
2018-02-12 13:28:37 +0800

    :lipstick: update header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -10,22 +10,38 @@ type Props = {
 export default class Header extends Component<Props> {
   props: Props;
 
-  render() {
+  render () {
     const { handleToggle } = this.props;
 
     return (
       <div className={styles.header}>
-        <div className={styles.menuTrigger}>
+        <div className={styles.sidebar}>
           <div className={styles['burger-wrapper']} onClick={handleToggle}>
-            <div className={styles.burger}></div>
+            <div className={styles.burger}/>
           </div>
         </div>
         <div className={styles.right}>
-          <div className={styles.controls}>
-            adfasdfasdf
+          <div className={styles['controls-track-info']}>
+            <div className={styles.thumb}></div>
+            <div className={styles.info}>
+              <div className={styles.title}>Friday Comes</div>
+              <div className={styles.artist}>Early</div>
+            </div>
+          </div>
+          <div className={styles['controls-mini-player']}>
+            <div className={styles['controls-button--backward']}>
+              <i className="fa fa-step-backward"/>
+            </div>
+            <div className={styles['controls-button--switch']}>
+              <i className="fa fa-play"/>
+              <i className="fa fa-pause"/>
+            </div>
+            <div className={styles['controls-button--forward']}>
+              <i className="fa fa-step-forward"/>
+            </div>
           </div>
           <div className={styles.menuTrigger}>
-            <i className="fa fa-bars" />
+            <i className="fa fa-list"/>
           </div>
         </div>
       </div>
